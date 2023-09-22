ed1e6d38bdc688ccd8705800f3bfd76845c70677
JackeyyLove <loidao99@gmail.com>
2023-09-22 22:46:21 +0700

    add navigation links

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -5,6 +5,7 @@ import HeaderComponent from './components/HeaderComponent'
 import FooterComponent from './components/FooterComponent'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import EmployeeComponent from './components/EmployeeComponent'
+import ListDepartmentComponent from './components/listDepartmentComponent'
 function App() {
   return (
     <>
@@ -14,7 +15,11 @@ function App() {
               <Route path ='/' element = {<ListEmployeeComponent/>}></Route>
               <Route path = '/employees' element = {<ListEmployeeComponent/>}></Route>
               <Route path ='/add-employee' element = {<EmployeeComponent/>}></Route>
+              {/* http://localhost:3030/edit-employee/1 */}
               <Route path ='/edit-employee/:id' element = {<EmployeeComponent/>}></Route>
+              
+              {/* http://localhost:3030/departments */}
+              <Route path ='/departments'element = {<ListDepartmentComponent/>}></Route>
           </Routes>
           <FooterComponent />   
       </BrowserRouter>
